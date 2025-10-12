82785bd64f0b24d16b7cfca7decb54d4f5e6290e
Mostafa Kamari <mostafamf555@gmail.com>
2025-10-12 11:03:50 +0330

    fixes bugs

--- src/app/(site routes)/capsules/[id]/page.tsx
+++ src/app/(site routes)/capsules/[id]/page.tsx
@@ -19,12 +19,28 @@ export async function generateMetadata({ params }: PageProps): Promise<Metadata>
     const capsule = res.data.capsule as Capsule;
 
     return {
+      metadataBase: new URL(process.env.PUBLIC_URL ?? 'http://localhost:3000'),
       title: `کپسول عمومی : ${capsule.title}` || 'کپسول عمومی',
       description: `توضیحات کپسول : ${capsule.description}` || 'توضیحات کپسول',
+      icons: [{ url: capsule.image || '/images/def.jpg' }],
       openGraph: {
+        url: `${process.env.PUBLIC_URL}/capsules/${capsuleId}` || `http://localhost:3000/capsules/${capsuleId}`,
         title: `کپسول عمومی : ${capsule.title}` || 'کپسول عمومی',
         description: `توضیحات کپسول : ${capsule.description}` || 'توضیحات کپسول',
-        images: [capsule.image || '/images/def.jpg'],
+        images: [
+          {
+            url: capsule.image || '/images/def.jpg',
+            width: 512,
+            height: 512,
+            alt: 'کپسول عمومی',
+          },
+        ],
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: 'سایت کپسول',
+        description: 'کپسول یه جور زمان‌نگهداره. خاطره‌هات رو با صدا، عکس یا نوشته می‌ذاری توش، زمان باز شدنش رو مشخص می‌کنی، و بعد... می‌مونه تا روزی که برگردی و دوباره بخونیش. برای خودت، برای یه عزیز، یا حتی واسه کسی که هنوز پیداش نکردی.',
+        images: ['/images/Logo.png'],
       },
     };
   } catch {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,13 @@ import { Toaster } from '@/components/ui/sonner';
 import ProgressBarLoading from './components/shared/Progressbar';
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.PUBLIC_URL ?? 'http://localhost:3000'),
   title: 'سایت کپسول',
   description: 'کپسول یه جور زمان‌نگهداره. خاطره‌هات رو با صدا، عکس یا نوشته می‌ذاری توش، زمان باز شدنش رو مشخص می‌کنی، و بعد... می‌مونه تا روزی که برگردی و دوباره بخونیش. برای خودت، برای یه عزیز، یا حتی واسه کسی که هنوز پیداش نکردی.',
   icons: [{ url: '/images/Logo.png' }],
   openGraph: {
     title: 'سایت کپسول',
+    url: process.env.PUBLIC_URL ?? 'http://localhost:3000',
     description: 'کپسول یه جور زمان‌نگهداره. خاطره‌هات رو با صدا، عکس یا نوشته می‌ذاری توش، زمان باز شدنش رو مشخص می‌کنی، و بعد... می‌مونه تا روزی که برگردی و دوباره بخونیش. برای خودت، برای یه عزیز، یا حتی واسه کسی که هنوز پیداش نکردی.',
     images: [
       {
@@ -20,8 +22,14 @@ export const metadata: Metadata = {
         alt: 'سایت کپسول',
       },
     ],
-    locale : 'fa_IR',
-    type : 'website'
+    locale: 'fa_IR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'سایت کپسول',
+    description: 'کپسول یه جور زمان‌نگهداره. خاطره‌هات رو با صدا، عکس یا نوشته می‌ذاری توش، زمان باز شدنش رو مشخص می‌کنی، و بعد... می‌مونه تا روزی که برگردی و دوباره بخونیش. برای خودت، برای یه عزیز، یا حتی واسه کسی که هنوز پیداش نکردی.',
+    images: ['/images/Logo.png'],
   },
 };
 
